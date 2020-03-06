37f32e13e0e068f799fbc068c0cf01da6977fe76
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 17:39:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,3 +34,13 @@ def map_to_square(source_array)
   return square_array
 end
 
+def reduce_to_total(source_array, starting_point)
+  i = 0 
+  while i < source_array.length do 
+      starting_point += source_array[i]
+      i += 1 
+    end
+    return starting_point
+  
+end
+
