0c74ffd47934606c1dbf242899d118a8518cae38
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 17:22:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,3 +13,13 @@ end
 def map_to_no_change(source_array)
   return source_array
 end
+
+def map_to_double(source_array)
+  doubled_array = []
+  i = 0 
+  while i < source_array.length do
+    doubled_array << source_array ** 2 
+    i += 1 
+  end
+  return doubled_array
+end 
