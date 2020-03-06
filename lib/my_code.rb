dbec818ffb5db24ee6b1eb905477e0db183c8975
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 17:25:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def map_to_double(source_array)
   doubled_array = []
   i = 0 
   while i < source_array.length do
-    doubled_array << source_array ^ 2 
+    doubled_array << source_array[i] ** 2 
     i += 1 
   end
   return doubled_array
