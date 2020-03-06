3428f4fd18d9fc16746fea891525edd5c5fcd8d1
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 17:13:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+
+map_to_negativize(source_array)
+  negative_values = []
+  i = 0 
+  while i < source_array.length do
+    negative_values << source_array[i] * -1
+    i += 1 
+
+  return negative_values
+end
