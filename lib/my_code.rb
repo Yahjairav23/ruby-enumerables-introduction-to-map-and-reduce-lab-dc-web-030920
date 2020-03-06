bfd0781e3e3840b0b738228b04d0cf3aefdf89e4
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 17:17:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,12 @@
 # My Code here....
 request 'pry'
-map_to_negativize(source_array)
-binding.pry
+
+def map_to_negativize(source_array)
   negative_values = []
   i = 0 
   while i < source_array.length do
     negative_values << source_array[i] * -1
     i += 1 
-  
+  end
   return negative_values
 end
